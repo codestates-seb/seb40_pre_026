45cb439ad3833cde3c4774f0b76bc850e496004d
jioneee <qoxx3659@gmail.com>
2022-10-26 14:00:09 +0900

    feat:네비게이션 기본 구조 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
+import logo from './logo.svg';
 import React from 'react';
 import Users from './components/Users';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
-
+import Nav from './components/Nav';
 function App() {
   return (
-    <div>
-      <Users></Users>
+    <div className="App">
+      <Nav></Nav>
     </div>
   );
 }
