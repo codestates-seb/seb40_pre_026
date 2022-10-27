878985e9d237bdaa1cc245e71fc10420d4561b22
nada1221 <nada931221@gmail.com>
2022-10-28 00:25:59 +0900

    feat: Connect router Home&Questions render

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -4,6 +4,7 @@ import Nav from '../components/Nav';
 import Footer from '../components/Footer';
 import { Sidebar } from '../components/Sidebar';
 import HomeRender from '../components/HomeRender';
+import { Routes, Route } from 'react-router-dom';
 import QuestionRenderPage from '../components/QuestionsRender';
 
 const MainPage = () => {
@@ -11,8 +12,11 @@ const MainPage = () => {
     <>
       <Nav />
       <Sidebar />
-      <HomeRender />
-      {/* <QuestionRenderPage /> */}
+      <Routes>
+        <Route path="/" element={<HomeRender />} />
+        {/* <Route path="/:id" element={<Details />} /> */}
+        <Route path="/questions" element={<QuestionRenderPage />} />
+      </Routes>
       <Footer />
     </>
   );
