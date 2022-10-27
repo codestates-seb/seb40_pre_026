e356704df50ecf83b73229948711e7492adc58e0
JoHyunho-clover <chlrh330@naver.com>
2022-10-27 10:29:50 +0900

    서버 환경 구성 및 간단한 로그인 회원가입

---
+++ server/src/main/java/com/codestates/preproject/PreprojectApplication.java
@@ -0,0 +1,15 @@
+package com.codestates.preproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class PreprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreprojectApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/com/codestates/preproject/util/CommonResponse.java
@@ -0,0 +1,18 @@
+package com.codestates.preproject.util;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+public class CommonResponse<T> extends BasicResponse{
+    private HttpStatus status;
+    private T body;
+
+    public CommonResponse(T data, HttpStatus status){
+        this.body = data;
+        this.status = status;
+    }
+
+}
