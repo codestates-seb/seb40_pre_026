d234745a58bd1b0a1f2efefb253b953446e87330
nada1221 <nada931221@gmail.com>
2022-10-28 10:15:38 +0900

    feat. Add route home

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -14,6 +14,10 @@ const MainPage = () => {
       <Sidebar />
       <Routes>
         <Route path="/" element={<HomeRender />} />
+
+        {/* Todo : 해당 path는 배포 이전에 수정작업을 통해 고쳐야 합니다 */}
+        <Route path="/codestates-seb/seb40_pre_026" element={<HomeRender />} />
+
         {/* <Route path="/:id" element={<Details />} /> */}
         <Route path="/questions" element={<QuestionRenderPage />} />
       </Routes>
